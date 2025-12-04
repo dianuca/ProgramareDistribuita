6de02836b86b187839fed90c5e5a59d87c81825a
dianaciodolan <dianamariascumpa@yahoo.com>
2025-12-04 09:18:10 +0200

    Problema 1 rezolvat cu exceptii v2

--- L1/ConversionCelsiusToFarheit.py
+++ L1/ConversionCelsiusToFarheit.py
@@ -1,7 +1,9 @@
-try:
-    c = input ("Introduceti temp in grade Celsius: ")
-    c = float(c)
-    f = c*9/5+32
-    print("Grade Farnheit ",f)
-except:
-    print("Eroare: introduceti un numar valid")
+while True:
+    try:
+        c = input ("Introduceti tmp in grade Celsius: ")
+        c = float(c)
+        break
+    except:
+        print("Input invalid! Va rog introduceti un numar.")
+f = c*9/5+32
+print("Grade Fahrenheit: ", f)
